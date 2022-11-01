bca458f9a2bcc27e5e29bd491ebe45d9849172d1
Username * <oueslatim268@gmail.com>
2022-11-01 16:13:45 +0100

    change command handler sys

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
+const fs = require("node:fs");
+const path = require("node:path");
 const { SlashCommandBuilder, Routes } = require("discord.js");
 const { REST } = require("@discordjs/rest");
 
-const commands = [
-  new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Replies with pong!"),
-].map((command) => command.toJSON());
+const commands = [];
+const commandsPath = path.join(__dirname, "src");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
@@ -18,5 +26,5 @@ rest
     ),
     { body: commands }
   )
-  .then(() => console.log("successfully registered application commands."))
+  .then(() => console.log("Successfully registered application commands."))
   .catch(console.error);
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const { Client, GatewayIntentBits } = require("discord.js");
 require("dotenv").config();
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const client = new Client({ intents: GatewayIntentBits.Guilds });
 
-const client = new Client({
-  intents: GatewayIntentBits.Guilds,
-});
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "src");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  client.commands.set(command.data.name, command);
+}
 
 client.once("ready", () => {
   console.log("Ready!");
@@ -12,10 +23,18 @@ client.once("ready", () => {
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  const { commandName } = interaction;
+  const command = interaction.client.commands.get(interaction.commandName);
 
-  if (commandName === "ping") {
-    await interaction.reply("Pong!");
+  if (!command) return;
+
+  try {
+    command.execute(interaction);
+  } catch (err) {
+    console.error(err);
+    interaction.reply({
+      content: "An error was found when executing this command!",
+      ephemeral: true,
+    });
   }
 });
 
