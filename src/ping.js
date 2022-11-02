0521abcfdca1c168c9a83fb790b980c142c20811
Username * <oueslatim268@gmail.com>
2022-11-02 12:24:56 +0100

    proper ping command

--- src/ping.js
+++ src/ping.js
@@ -5,6 +5,19 @@ module.exports = {
     .setName("ping")
     .setDescription('returns "pong!"'),
   async execute(interaction) {
-    await interaction.reply("pong!");
+    let totalSeconds = interaction.client.uptime / 1000;
+    let days = Math.floor(totalSeconds / 86400);
+    totalSeconds %= 86400;
+    let hours = Math.floor(totalSeconds / 3600);
+    totalSeconds %= 3600;
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = Math.floor(totalSeconds % 60);
+
+    let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+
+    await interaction.reply(`
+      > WebSocket ping: ${interaction.client.ws.ping}ms.
+      > Uptime: ${uptime}
+      `);
   },
 };
