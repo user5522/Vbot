d8f0c3e2a4d04c654ab7b595c1b9eee21629e73a
Username * <oueslatim268@gmail.com>
2022-12-20 12:39:40 +0100

    serverinfo/icon command rework & improvements

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const {
   SlashCommandBuilder,
   EmbedBuilder,
   ChannelType,
+  Message,
 } = require("discord.js");
 
 module.exports = {
@@ -19,99 +20,111 @@ module.exports = {
         .setDescription("returns the current server's icon")
     ),
   async execute(interaction) {
-    if (interaction.options.getSubcommand() === "info") {
-      const guild = interaction.guild;
-      const guildName = guild.name;
-      const guildID = guild.id;
-      const guildCreationDate = Math.floor(guild.createdTimestamp / 1000);
-      const guildOwner = await guild.fetchOwner();
-      const totalMemberCount = await guild.memberCount;
-      const memberCount = guild.members.cache.filter(
-        (member) => !member.user.bot
-      ).size;
-      const botCount = guild.members.cache.filter(
-        (member) => member.user.bot
-      ).size;
-      const textChannels = guild.channels.cache.filter(
-        (c) => c.type === ChannelType.GuildText
-      ).size;
-      const voiceChannels = guild.channels.cache.filter(
-        (c) => c.type === ChannelType.GuildVoice
-      ).size;
-      const allChannelCount = guild.channels.cache.filter(
-        (c) =>
-          c.type === ChannelType.GuildText || c.type === ChannelType.GuildVoice
-      ).size;
+    const guild = interaction.guild;
+    const guildName = guild.name;
+    const guildID = guild.id;
+    const guildCreationDate = Math.floor(guild.createdTimestamp / 1000);
+    const guildOwner = await guild.fetchOwner();
 
-      const guildRoleCount = guild.roles.cache.size;
-      const totalGuildBoosts = guild.premiumSubscriptionCount;
-      const guildBoostTier = guild.premiumTier;
-      const guildIcon = guild.iconURL({ dynamic: true });
+    switch (interaction.options.getSubcommand()) {
+      case "info": {
+        const memberCount = getMemberCount(guild);
+        const channelCount = getChannelCount(guild);
+        const roleCount = getRoleCount(guild);
 
-      let rolemap = guild.roles.cache
-        .sort((a, b) => b.position - a.position)
-        .map((r) => r)
-        .join(",");
-      if (rolemap.length > 1024) rolemap = "To many roles to display";
-      if (!rolemap) rolemap = "No roles";
+        const embed = new EmbedBuilder()
+          .setTitle(`${guildName} - Server Info`)
+          .setColor("#000000")
+          .setThumbnail(guild.iconURL({ dynamic: true }))
+          .addFields(
+            { name: "Server ID", value: `${guildID}`, inline: true },
+            { name: "Owner", value: `${guildOwner}`, inline: true },
+            {
+              name: "Server Creation Date",
+              value: `<t:${guildCreationDate}>`,
+              inline: true,
+            },
+            {
+              name: `Member Count - ${memberCount.total}`,
+              value: `
+              Members: ${memberCount.members}
+              Bots: ${memberCount.bots}
+              `,
+              inline: true,
+            },
+            {
+              name: `Channels - ${channelCount.total}`,
+              value: `
+              Text: ${channelCount.text}
+              Voice: ${channelCount.voice}
+              `,
+              inline: true,
+            },
+            {
+              name: `Roles - ${roleCount}`,
+              value: `${getRoleList(guild).join(", ")}`,
+              inline: true,
+            },
+            {
+              name: `Total Server Boosts`,
+              value: `${guild.premiumSubscriptionCount}`,
+              inline: true,
+            },
+            {
+              name: `Server Boost Tier`,
+              value: `${guild.premiumTier}/3`,
+              inline: true,
+            }
+          );
+        await interaction.channel.send({ embeds: [embed] });
+        break;
+      }
+      case "icon": {
+        let embed;
+        embed = new EmbedBuilder()
+          .setTitle(`${guildName} - Server Icon`)
+          .setColor("#000000")
+          .setImage(guild.iconURL({ dynamic: true }));
 
-      const embed = new EmbedBuilder()
-        .setTitle(`${guildName} - Server Info`)
-        .setColor("#000000")
-        .setThumbnail(guildIcon)
-        .addFields(
-          { name: "Server ID", value: `${guildID}`, inline: true },
-          { name: "Owner", value: `${guildOwner}`, inline: true },
-          {
-            name: "Server Creation Date",
-            value: `<t:${guildCreationDate}>`,
-            inline: true,
-          },
-          {
-            name: `Member Count - ${totalMemberCount}`,
-            value: `
-            Members: ${memberCount}
-            Bots: ${botCount}
-            `,
-            inline: true,
-          },
-          {
-            name: `Channels - ${allChannelCount}`,
-            value: `
-            Text: ${textChannels}
-            Voice: ${voiceChannels}
-            `,
-            inline: true,
-          },
-          {
-            name: `Roles - ${guildRoleCount}`,
-            value: `${rolemap.split(",").join(" ")}`,
-            inline: true,
-          },
-          {
-            name: `Total Server Boosts - ${totalGuildBoosts}`,
-            value: "\u200b",
-            inline: true,
-          },
-          {
-            name: `Server Boost Tier - ${guildBoostTier}/3`,
-            value: "\u200b",
-            inline: true,
-          }
+        await interaction.channel.send({ embeds: [embed] });
+        break;
+      }
+      default: {
+        await interaction.channel.send(
+          "Invalid subcommand. Use `/server info` or `/server icon`."
         );
-      await interaction.reply({ embeds: [embed] });
-    } else if (interaction.options.getSubcommand() === "icon") {
-      const guild = interaction.guild;
-      const guildName = guild.name;
-      const guildIcon = guild.iconURL({ dynamic: true });
-
-      const embed = new EmbedBuilder()
-        .setTitle(`${guildName} - Server Icon`)
-        .setColor("#000000")
-        .setImage(guildIcon);
-      await interaction.reply({
-        embeds: [embed],
-      });
+      }
     }
   },
 };
+
+function getMemberCount(guild) {
+  return {
+    total: guild.memberCount,
+    members: guild.members.cache.filter((member) => !member.user.bot).size,
+    bots: guild.members.cache.filter((member) => member.user.bot).size,
+  };
+}
+
+function getChannelCount(guild) {
+  return {
+    total: guild.channels.cache.filter(
+      (c) =>
+        c.type === ChannelType.GuildText || c.type === ChannelType.GuildVoice
+    ).size,
+    text: guild.channels.cache.filter((c) => c.type === ChannelType.GuildText)
+      .size,
+    voice: guild.channels.cache.filter((c) => c.type === ChannelType.GuildVoice)
+      .size,
+  };
+}
+
+function getRoleCount(guild) {
+  return guild.roles.cache.size;
+}
+
+function getRoleList(guild) {
+  return guild.roles.cache
+    .sort((a, b) => b.position - a.position)
+    .map((r) => r);
+}
