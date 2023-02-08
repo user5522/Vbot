0deeae3793e4a745d96e09269507eaa4d7e0d459
Username * <oueslatim268@gmail.com>
2023-02-08 21:37:46 +0100

    ping cmd improvements

--- src/modules/ping.js
+++ src/modules/ping.js
@@ -3,17 +3,40 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription('returns "pong!"'),
+    .setDescription("returns information about the bot"),
   async execute(interaction) {
     try {
       const startTime = interaction.client.readyAt;
       const currentTime = new Date();
       const elapsedTime = (currentTime - startTime) / 1000;
 
+      let seconds = Math.floor(elapsedTime % 60);
+      let minutes = Math.floor((elapsedTime / 60) % 60);
+      let hours = Math.floor((elapsedTime / 3600) % 24);
+      let days = Math.floor(elapsedTime / 86400);
+
+      let uptime = "";
+      if (days > 0) {
+        uptime += `${days}d `;
+      }
+      if (hours > 0) {
+        uptime += `${hours}h `;
+      }
+      if (minutes > 0) {
+        uptime += `${minutes}m `;
+      }
+      if (seconds > 0) {
+        uptime += `${seconds}s`;
+      }
+      let ping = `${interaction.client.ws.ping}ms`;
+      if (interaction.client.ws.ping > 100) {
+        ping += " :warning:";
+      }
+
       await interaction.reply(`
-        > WebSocket ping: ${interaction.client.ws.ping}ms
-        > Up time: ${elapsedTime}s
-      `);
+        > WebSocket ping: ${ping}
+        > Up time: ${uptime}
+        `);
     } catch (error) {
       await interaction.reply("An error occurred while executing the command.");
       console.error(error);
